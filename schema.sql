bea55812d022538bcc1f045c3b75b1fb170f044e
denis-gerc <gdv1001@gmail.com>
2020-06-01 20:39:15 +0300

    add schema.sql

---
+++ schema.sql
@@ -0,0 +1,44 @@
+-- Создаем БД
+CREATE DATABASE doingsdone DEFAULT CHARACTER
+SET utf8 DEFAULT COLLATE utf8_general_ci -- Делаем ее активной для работы
+USE doingsdone -- Создаем табллицу для сущности Пользователь
+CREATE TABLE user_reg (
+      user_id INT(11) UNSIGNED AUTO_INCREMENT NOT NULL,
+      date_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      email CHAR (64) NOT NULL UNIQUE,
+      us_name VARCHAR (128) NOT NULL,
+      pass CHAR (64) NOT NULL,
+      PRIMARY KEY (user_id)
+) ENGINE = InnoDB;
+-- Создаем табллицу для сущности Проект
+CREATE TABLE project (
+      proj_id INT(11) UNSIGNED AUTO_INCREMENT NOT NULL,
+      user_id INT (11) UNSIGNED NOT NULL,
+      proj_name VARCHAR (128) NOT NULL,
+      PRIMARY KEY (proj_id),
+      INDEX idxProject (user_id),
+      CONSTRAINT user_project FOREIGN KEY (user_id) REFERENCES user_reg (user_id) ON
+            DELETE CASCADE ON
+            UPDATE CASCADE
+) ENGINE = InnoDB;
+-- Создаем табллицу для сущности Задача
+CREATE TABLE task (
+      task_id INT(11) UNSIGNED AUTO_INCREMENT NOT NULL,
+      proj_id INT (11) UNSIGNED NOT NULL,
+      user_id INT (11) UNSIGNED NOT NULL,
+      date_task TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      status_task INT (1) UNSIGNED NOT NULL DEFAULT '0',
+      CHECK (status_task <= 1),
+      title_task VARCHAR (255) NOT NULL,
+      link_file VARCHAR (255) NOT NULL,
+      date_task_end TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (task_id),
+      INDEX idxTaskProject (proj_id),
+      INDEX idxTaskUser (user_id),
+      CONSTRAINT project_task FOREIGN KEY (proj_id) REFERENCES project (proj_id) ON
+                  DELETE CASCADE ON
+                  UPDATE CASCADE,
+      CONSTRAINT user_task FOREIGN KEY (user_id) REFERENCES user_reg (user_id) ON
+                  DELETE CASCADE ON
+                  UPDATE CASCADE
+) ENGINE = InnoDB;
