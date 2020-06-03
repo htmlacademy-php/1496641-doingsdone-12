72fee1addaa725b3d3cdaa95669b1b151bdc8986
denis-gerc <gdv1001@gmail.com>
2020-06-03 21:42:01 +0300

    Пишем SQL-запросы

---
+++ queries.sql
@@ -0,0 +1,78 @@
+-- Добавляем пользователей
+INSERT INTO user_reg (date_reg, email, us_name, pass)
+VALUES (DEFAULT, 'ivanov@yandex.ru', 'Иван', 'qwerty'),
+    (DEFAULT, 'petrov@mail.ru', 'Петр', 'asdfgh'),
+    (DEFAULT, 'sidorov@mail.ru', 'Сидор', 'zxcvbnm'),
+    (DEFAULT, 'pupkin@mail.ru', 'Пупкин', 'ytrewq');
+-- Добавляем список проектов
+INSERT INTO project (user_id, proj_name)
+VALUES (1, 'Входящие'),
+    (2, 'Учеба'),
+    (3, 'Работа'),
+    (4, 'Домашние дела'),
+    (1, 'Авто');
+-- Добавляем список задач
+-- 01
+INSERT INTO task
+VALUES (
+        DEFAULT,
+        1,
+        1,
+        DEFAULT,
+        DEFAULT,
+        'Собеседование в IT компании',
+        'http//download/file01.jpg',
+        '2020-01-05'
+    );
+-- 02
+INSERT INTO task
+VALUES (
+        DEFAULT,
+        2,
+        2,
+        DEFAULT,
+        DEFAULT,
+        'Выполнить тестовое задание',
+        'http//download/file02.jpg',
+        '2020-02-15'
+    );
+-- 03
+INSERT INTO task
+VALUES (
+        DEFAULT,
+        3,
+        3,
+        DEFAULT,
+        1,
+        'Сделать задание первого раздела',
+        'http//download/file03.jpg',
+        '2020-03-25'
+    );
+-- 04
+INSERT INTO task
+VALUES (
+        DEFAULT,
+        4,
+        4,
+        DEFAULT,
+        DEFAULT,
+        'Встреча с другом',
+        NULL,
+        '2020-04-05'
+    );
+-- query #1. Получить список из всех проектов для одного пользователя
+SELECT proj_name
+FROM project
+WHERE user_id = 1;
+-- query #2. Получить список из всех задач для одного проекта
+SELECT title_task
+FROM task
+WHERE proj_id = 1;
+-- query #3. Пометить задачу как выполненную
+UPDATE task
+SET status_task = 1
+WHERE status_task = 0;
+-- query #4. Обновить название задачи по её идентификатору
+UPDATE task
+SET title_task = 'Новая задача'
+WHERE task_id = 2;
